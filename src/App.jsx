9f19524df4a341b61fb1ef7744e8d33c318367a9
ptrv-dev <petrov99119@gmail.com>
2023-05-14 23:29:42 +0300

    cleanup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+  return (
+    <div className="container">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
